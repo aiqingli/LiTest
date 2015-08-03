6604421ca960d821e3bfd2b7e71ebdaf8c1c0ea2
Aiqing Li <liaiqi@LIAIQI2.americas.hpqcorp.net>
2015-08-03 09:58:08 -0400

    test 813

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -2,7 +2,7 @@ package com.solo;
 
 public class TestA {
     public static void main(String[] args){
-        System.out.println("This is just a test");
+        System.out.println("This is just a test, please ignore!");
     }
 
 }
