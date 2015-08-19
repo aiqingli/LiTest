58ff507d7b8678bdd55246ac885376377051690e
Aiqing Li <liaiqi@LIAIQI2.americas.hpqcorp.net>
2015-08-19 12:05:35 -0400

    2nd commit on branch819

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -4,6 +4,7 @@ public class TestA {
     public static void main(String[] args){
         System.out.println("2nd commit on my second branch on 0819!");
         System.out.println("1st commit on branch819!");
+        System.out.println("2nd commit on branch819!");
     }
 
 }
