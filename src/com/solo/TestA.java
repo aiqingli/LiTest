87ca80bd5117a6fbd0d3d34e20e539c486a89649
Aiqing Li <liaiqi@LIAIQI2.americas.hpqcorp.net>
2015-08-19 12:15:06 -0400

    just checking things

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -2,7 +2,7 @@ package com.solo;
 
 public class TestA {
     public static void main(String[] args){
-        System.out.println("just checking!");
+        System.out.println("just checking things!");
     }
 
 }
