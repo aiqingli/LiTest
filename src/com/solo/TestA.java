5eea1b622c65db71b9f10048e41e63d5374dfc72
Aiqing <aiqing_li@yahoo.com>
2021-10-28 17:08:47 -0500

    uat on master.

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -2,6 +2,6 @@ package com.solo;
 
 public class TestA {
     public static void main(String[] args){
-        System.out.println("uat on dev just checking things!!!");
+        System.out.println("uat on master just checking things!!!");
     }
 }
