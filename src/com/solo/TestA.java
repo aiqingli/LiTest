c5cce452611c82d3ce59ddec27af1ecfbd640059
Aiqing <aiqing_li@yahoo.com>
2021-10-28 17:16:19 -0500

    uat has master changes now.

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -3,6 +3,6 @@ package com.solo;
 public class TestA {
     public static void main(String[] args){
         System.out.println("master just checking things!");
-        System.out.println("uat just checking things!!!");
+        System.out.println("uat just checking things!");
     }
 }
