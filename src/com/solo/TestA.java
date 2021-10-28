5cd614cf89b80b35637245644d63873aa7dbe0fd
Aiqing <aiqing_li@yahoo.com>
2021-10-28 16:49:41 -0500

    Deleted last two lines.

--- src/com/solo/TestA.java
+++ src/com/solo/TestA.java
@@ -4,8 +4,5 @@ public class TestA {
     public static void main(String[] args){
         System.out.println("just checking things!");
         System.out.println("2nd commit on my second branch on 0819!");
-        System.out.println("1st commit on branch819!");
-        System.out.println("2nd commit on branch819!");
     }
-
 }
